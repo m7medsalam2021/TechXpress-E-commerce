3bc129f66355a53a78a9fdc9e5722734421e9aee
Sallam <m7medsalam@gmail.com>
2025-04-26 20:09:52 +0300

    Update This Project

---
+++ TechXpress-E-commerce/MVC.Domain/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVC.Domain.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+    }
+}
