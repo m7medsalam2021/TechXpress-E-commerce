7282dcef4b5ead0a5096a596c19c464839c40dd4
Ibrahim-Suker <ibrahimsukeroo@gmail.com>
2025-05-07 01:36:26 +0300

    Image

--- Domain/Models/Category.cs
+++ Domain/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -13,6 +14,8 @@ namespace Domain.Models
         [Required]
         public string Name { get; set; }
         public string Description { get; set; }
+
+        [ValidateNever]
         public string Image { get; set; }
     }
 }
